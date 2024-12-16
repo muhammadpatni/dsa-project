6ad633332d2a959227d4e35ead00e9af05ec5ccc
Kamran Khan <Kami0368@outlook.com>
2024-12-16 21:41:21 +0500

    Add the name

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello, world!");
         System.out.println("haram");
         System.out.println("muhammad");
+        System.out.println("kamran");
     }
 }
