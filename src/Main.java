a88759228f2ee518886598ff78c5a0742154346f
HP <HP@DESKTOP-7TMAKUL>
2024-12-16 21:46:47 +0500

    m

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         System.out.println("Hello, world!");
         System.out.println("haram");
         System.out.println("muhammad");
+        System.out.println("patni");
+
     }
 }
